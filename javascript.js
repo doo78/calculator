19ac5623c7ffb9b5b19cb208fea1e42439691793
doo78 <rickydeangordon@gmail.com>
2024-08-31 19:34:55 +0100

    I made it interactive but the calculation is wrong when you continue using it after pressing equals

--- javascript.js
+++ javascript.js
@@ -16,34 +16,100 @@ function divide (num1, num2){
 }
 
 function operate(num1, num2, operator){
+
     if (operator == "+"){
-        console.log(add(num1, num2));
+        display.textContent = add(num1, num2);
     }
     
     else if (operator == "-"){
-        console.log(minus(num1, num2));
+        display.textContent = minus(num1, num2);
     }
     
     else if (operator == "/"){
-        console.log(divide(num1, num2));
+        display.textContent = divide(num1, num2);
     }
     
     else if (operator == "*"){
-        console.log(multiply(num1, num2));
+        display.textContent = multiply(num1, num2);
     }
     
     else{
         console.log("You didn't enter the right operator. Now you sit here all sad.")
     }
+
+    currentNumber = 1;
+
+    num1 = display.textContent
+
+    num2 = "";
+
+    operator = "";
+
+    console.log("num1: " + num1);
+    console.log("num2: " + num2);
+    
+    
 }
 
-/*
-let num1 = parseInt(prompt("Enter number 1: "));
+const display = document.querySelector("#display");
 
-let num2 = parseInt(prompt("Enter number 2: "))
+const buttons = document.querySelectorAll("button");
 
-let operator = prompt("Enter the operator (+, -, /, *): ")
+let num1 = "";
 
-operate(num1, num2, operator);
-*/
+let num2 = "";
+
+let currentNumber = 1;
+
+let operator = "";
+
+function clear(){
+
+    num1 = "";
+
+    num2 = "";
+
+    currentNumber = 1;
+
+    operator = "";
+
+    display.textContent = "";
+}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+
+      display.textContent += button.textContent;
+
+      if (button.textContent == "="){
+        if (operator != ""){
+            operate (parseInt(num1), parseInt(num2), operator);
+        }
+      }
+
+      else if (button.textContent == "CLR"){
+        clear();
+      }
+
+      else if (("*/+-").includes(button.textContent)){
+        operator = button.textContent;
+        currentNumber = 2;
+        num2 = "";
+      }
+
+      else{
+
+        if (currentNumber == 1){
+            console.log("num1 before: " + num1);
+            num1 += button.textContent;
+            console.log("num1 after: " + num1);
+        }
+        else{
+            console.log("num2 before: " + num2);
+            num2 += button.textContent;
+            console.log("num2 after: " + num2);
+        }
+      }
+    });
+  });
 
